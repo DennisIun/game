a76b97f5b51fccbef53031a85521cbb16493e46c
Dennis Iun <dennisiun@hotmail.com>
2023-02-01 10:40:55 +0800

    Initial Commit

---
+++ game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  game
+//
+//  Created by Dennis Iun on 1/2/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
